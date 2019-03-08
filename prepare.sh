cc19fba40cd6f98fbbd95febcb5f8aa888ae20a1
Zhen Zhang <izgzhen@gmail.com>
2019-03-08 10:43:53 -0800

    use python2

---
+++ prepare.sh
@@ -0,0 +1,5 @@
+pip install antlr4-python2-runtime
+sudo apt-get install lua-torch-torch7 torch-trepl
+export CODENN_WORK=/data/datasets/codenn/workdir
+export CODENN_DIR=$PWD
+export PYTHONPATH=$PWD/src:$PWD/src/sqlparse
