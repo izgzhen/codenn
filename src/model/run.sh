ae46ed60fbb1136d5b20098ea5c00ed60d41da30
Zhen Zhang <izgzhen@gmail.com>
2019-03-17 18:58:41 -0700

    expose decay param

--- src/model/run.sh
+++ src/model/run.sh
@@ -5,7 +5,7 @@ GPUIDX=1
 BEAMSIZE=10
 
 # Run Training
-th main.lua -dev_ref_file $CODENN_DIR/data/stackoverflow/${LANGUAGE}/dev/ref.txt -gpuidx $GPUIDX -language $1
+th main.lua -decay $DECAY -dev_ref_file $CODENN_DIR/data/stackoverflow/${LANGUAGE}/dev/ref.txt -gpuidx $GPUIDX -language $1
 
 # Run prediction
 th predict.lua -encoder ${LANGUAGE}.encoder -decoder ${LANGUAGE}.decoder -beamsize $BEAMSIZE -gpuidx $GPUIDX -language ${LANGUAGE}
